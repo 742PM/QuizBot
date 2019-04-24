38fb711654a68095ad07d54df9f38f2d49edace3
insomnia* <budljanskiy@mail.ru>
2019-04-24 19:31:07 +0500

    add logger into UpdateController;

--- QuizBot/QuizWebHookBot/Controllers/UpdateController.cs
+++ QuizBot/QuizWebHookBot/Controllers/UpdateController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using QuizRequestService;
 using QuizWebHookBot.Services;
 using Telegram.Bot.Types;
@@ -13,23 +14,30 @@ namespace QuizWebHookBot.Controllers
         private readonly IBotService botService;
         private readonly IUpdateService updateService;
         private readonly IQuizService quizService;
+        private readonly ILogger logger;
 
-        public UpdateController(IUpdateService updateService, IBotService botService, IQuizService quizService)
+        public UpdateController(IUpdateService updateService, IBotService botService, IQuizService quizService, 
+            ILogger logger)
         {
             this.updateService = updateService;
             this.botService = botService;
             this.quizService = quizService;
+            this.logger = logger;
         }
 
         // POST api/update
         [HttpPost]
         public async Task Post([FromBody] Update update)
         {
+            logger.LogInformation("Entering POST request");
+            logger.LogInformation($"Update Type: {update.Type}");
             if (update.Type == UpdateType.CallbackQuery)
             {
+                logger.LogInformation("CallbackQuery processing");
                 await botService.Client.SendTextMessageAsync(
                     update.Message.Chat.Id,
                     $"Received {update.CallbackQuery.Data}");
+                logger.LogInformation("CallbackQuery processed");
                 return;
             }
 
@@ -40,8 +48,10 @@ namespace QuizWebHookBot.Controllers
             
             if (message.Type == MessageType.Text)
             {
+                logger.LogInformation("TextMessage processing");
                 var userCommand = updateService.ProcessMessage(message);
                 await userCommand.ExecuteAsync(message.Chat, botService.Client, quizService);
+                logger.LogInformation("TextMessage processed");
             }
         }
     }
