235a12158aed223d5044e8e6353790614c0ddbc7
insomnia* <budljanskiy@mail.ru>
2019-04-27 08:12:58 +0500

    progress add;

--- QuizBot/QuizBotCore/Commands/CheckTaskCommand.cs
+++ QuizBot/QuizBotCore/Commands/CheckTaskCommand.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using QuizBotCore.Commands;
 using QuizBotCore.Database;
+using QuizBotCore.States;
 using QuizRequestService;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -21,6 +23,7 @@ namespace QuizBotCore
             IUserRepository userRepository, ILogger logger)
         {
             var user = userRepository.FindByTelegramId(chat.Id);
+            var state = user.CurrentState as TaskState;
             logger.LogInformation("Command: CheckTask");
             logger.LogInformation($"User: ID: {user.Id} TG:{user.TelegramId}");
             var isCorrect = quizService.SendAnswer(user.Id, answer);
@@ -28,7 +31,12 @@ namespace QuizBotCore
             {
                 logger.LogInformation($"The answer is {answer} and {isCorrect}");
                 if (isCorrect.Value)
+                {
                     await client.SendTextMessageAsync(chat.Id, "А ты прав!");
+                    var progress = quizService.GetCurrentProgress(user.Id, 
+                        Guid.Parse(state.TopicId), Guid.Parse(state.LevelId));
+                    await client.SendTextMessageAsync(chat.Id, $"Прогресс:\n{progress}");
+                }
                 else await client.SendTextMessageAsync(chat.Id, "Подумай еще(");
             }
             else
