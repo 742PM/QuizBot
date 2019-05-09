339515dee1d0a705fb59683a1b4d5950de5fb88b
insomnia* <budljanskiy@mail.ru>
2019-05-09 18:14:13 +0500

    Tests: Changed check on types;

--- QuizBot/QuizBotCoreTests/TelegramStateMachineTests.cs
+++ QuizBot/QuizBotCoreTests/TelegramStateMachineTests.cs
@@ -20,16 +20,17 @@ namespace QuizBotCoreTests
         private IStateMachine<ICommand> stateMachine;
         private TopicDTO testTopic;
         private LevelDTO testLevel;
+
         [SetUp]
         public void Setup()
         {
             quizService = A.Fake<IQuizService>();
             testTopic = new TopicDTO(Guid.NewGuid(), "TestTopicName");
             testLevel = new LevelDTO(Guid.NewGuid(), "TestLevelName");
-            A.CallTo(() => quizService.GetTopics()).Returns(new []{testTopic});
+            A.CallTo(() => quizService.GetTopics()).Returns(new[] {testTopic});
             A.CallTo(() => quizService.GetLevels(testTopic.Id)).Returns(new[] {testLevel});
-            userRepository = A.Fake<IUserRepository>(); 
-            stateMachine = new TelegramStateMachine(quizService,userRepository);
+            userRepository = A.Fake<IUserRepository>();
+            stateMachine = new TelegramStateMachine(quizService, userRepository);
         }
 
         [Test]
@@ -38,61 +39,62 @@ namespace QuizBotCoreTests
             var unknownUserState = new UnknownUserState();
             var helpTransition = new HelpTransition();
             var (state, command) = stateMachine.GetNextState(unknownUserState, helpTransition);
-            state.GetType().Should().Be(typeof(TopicSelectionState));
-            command.GetType().Should().Be(typeof(SelectTopicCommand));
+            state.Should().BeOfType<TopicSelectionState>();
+            command.Should().BeOfType<SelectTopicCommand>();
         }
-        
+
         [Test]
         public void GetNextState_FeedbackTransition_ReturnTopicSelectionState()
         {
             var unknownUserState = new UnknownUserState();
             var feedbackTransition = new FeedbackTransition();
             var (state, command) = stateMachine.GetNextState(unknownUserState, feedbackTransition);
-            state.GetType().Should().Be(typeof(TopicSelectionState));
-            command.GetType().Should().Be(typeof(FeedBackCommand));
+            state.Should().BeOfType<TopicSelectionState>();
+            command.Should().BeOfType<FeedBackCommand>();
         }
-        
+
         [Test]
         public void GetNextState_UnknownState_ReturnTopicSelectionState()
         {
             var unknownUserState = new UnknownUserState();
             var backTransition = new BackTransition();
             var (state, command) = stateMachine.GetNextState(unknownUserState, backTransition);
-            state.GetType().Should().Be(typeof(TopicSelectionState));
-            command.GetType().Should().Be(typeof(SelectTopicCommand));
+            state.Should().BeOfType<TopicSelectionState>();
+            command.Should().BeOfType<SelectTopicCommand>();
         }
-        
+
         [Test]
         public void GetNextState_TopicSelectionState_OnBackTransition_ReturnTopicSelectionState()
         {
             var unknownUserState = new TopicSelectionState();
             var backTransition = new BackTransition();
             var (state, command) = stateMachine.GetNextState(unknownUserState, backTransition);
-            state.GetType().Should().Be(typeof(TopicSelectionState));
-            command.GetType().Should().Be(typeof(SelectTopicCommand));
+            state.Should().BeOfType<TopicSelectionState>();
+            command.Should().BeOfType<SelectTopicCommand>();
         }
-        
+
         [Test]
         public void GetNextState_TopicSelectionState_OnCorrectTransition_ReturnTopicSelectionState()
         {
             var unknownUserState = new TopicSelectionState();
             var backTransition = new CorrectTransition(testTopic.Id.ToString());
             var (state, command) = stateMachine.GetNextState(unknownUserState, backTransition);
-            state.As<LevelSelectionState>().TopicDto.Should().Be(new LevelSelectionState(testTopic).TopicDto);
-            command.GetType().Should().Be(typeof(SelectLevelCommand));
+            state.As<LevelSelectionState>()
+                .TopicDto.Should()
+                .Be(new LevelSelectionState(testTopic).TopicDto);
+            command.Should().BeOfType<SelectLevelCommand>();
         }
-        
+
         [Test]
-        
         public void GetNextState_LevelSelectionState_OnBackTransition_ReturnTopicSelectionState()
         {
             var unknownUserState = new LevelSelectionState(testTopic);
             var backTransition = new BackTransition();
             var (state, command) = stateMachine.GetNextState(unknownUserState, backTransition);
-            state.GetType().Should().Be(typeof(TopicSelectionState));
-            command.GetType().Should().Be(typeof(SelectTopicCommand));
+            state.Should().BeOfType<TopicSelectionState>();
+            command.Should().BeOfType<SelectTopicCommand>();
         }
-        
+
         [Test]
         public void GetNextState_LevelSelectionState_OnCorrectTransition_ReturnTopicSelectionState()
         {
@@ -102,9 +104,9 @@ namespace QuizBotCoreTests
             var taskState = state.As<TaskState>();
             taskState.TopicDto.Should().Be(testTopic);
             taskState.LevelDto.Should().Be(testLevel);
-            command.GetType().Should().Be(typeof(ShowTaskCommand));
+            command.Should().BeOfType<ShowTaskCommand>();
         }
-        
+
         [Test]
         public void GetNextState_TaskState_OnBackTransition_ReturnLevelSelectionState()
         {
@@ -113,9 +115,9 @@ namespace QuizBotCoreTests
             var (state, command) = stateMachine.GetNextState(unknownUserState, backTransition);
             var levelSelectionState = state.As<LevelSelectionState>();
             levelSelectionState.TopicDto.Should().Be(testTopic);
-            command.GetType().Should().Be(typeof(SelectLevelCommand));
-        } 
-        
+            command.Should().BeOfType<SelectLevelCommand>();
+        }
+
         [Test]
         public void GetNextState_TaskState_OnShowHintTransition_ReturnShowHintCommandAndSaveState()
         {
@@ -125,21 +127,19 @@ namespace QuizBotCoreTests
             var taskState = state.As<TaskState>();
             taskState.TopicDto.Should().Be(testTopic);
             taskState.LevelDto.Should().Be(testLevel);
-            command.GetType().Should().Be(typeof(ShowHintCommand));
+            command.Should().BeOfType<ShowHintCommand>();
         }
-        
+
         [Test]
         public void GetNextState_TaskState_OnCorrectTransition_ReturnCheckTaskCommandAndSaveState()
         {
-            var unknownUserState = new TaskState(testTopic,testLevel);
+            var unknownUserState = new TaskState(testTopic, testLevel);
             var backTransition = new CorrectTransition("answer");
             var (state, command) = stateMachine.GetNextState(unknownUserState, backTransition);
             var taskState = state.As<TaskState>();
             taskState.TopicDto.Should().Be(testTopic);
             taskState.LevelDto.Should().Be(testLevel);
-            command.GetType().Should().Be(typeof(CheckTaskCommand));
+            command.Should().BeOfType<CheckTaskCommand>();
         }
-        
-
     }
 }
