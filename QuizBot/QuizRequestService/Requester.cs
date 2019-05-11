dd10c861a7e44d509c6239d2972269dcff420aa0
insomnia* <budljanskiy@mail.ru>
2019-05-11 17:02:41 +0500

    currentProgress changed on progress;

--- QuizBot/QuizRequestService/Requester.cs
+++ QuizBot/QuizRequestService/Requester.cs
@@ -46,7 +46,7 @@ namespace QuizRequestService
 
         public ProgressDTO GetCurrentProgress(Guid userId, Guid topicId, Guid levelId)
         {
-            var client = new RestClient(serverUri + $"/api/{userId}/{topicId}/{levelId}/currentProgress");
+            var client = new RestClient(serverUri + $"/api/{userId}/{topicId}/{levelId}/progress");
             var request = SendGetRequest(client, Method.GET);
             if (request.StatusCode == HttpStatusCode.OK)
                 return JsonConvert.DeserializeObject<ProgressDTO>(request.Content);
