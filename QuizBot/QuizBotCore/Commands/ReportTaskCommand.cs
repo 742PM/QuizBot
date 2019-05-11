dd90259b44701b6b34b82bb52b922fc0c8fe5f36
insomnia* <budljanskiy@mail.ru>
2019-05-11 19:54:01 +0500

    report message testing;

--- QuizBot/QuizBotCore/Commands/ReportTaskCommand.cs
+++ QuizBot/QuizBotCore/Commands/ReportTaskCommand.cs
@@ -17,8 +17,8 @@ namespace QuizBotCore
         public async Task ExecuteAsync(Chat chat, TelegramBotClient client, ServiceManager serviceManager)
         {
             long to = 349845203;
-            await client.SendTextMessageAsync(to, "Опишите вашу проблему:", replyMarkup: new ForceReplyMarkup(), replyToMessageId: messageId);
-//            await client.ForwardMessageAsync("@quibblereport", chat.Id, messageId);
+            await client.ForwardMessageAsync("@quibblereport", chat.Id, messageId);
+            await client.SendTextMessageAsync("@quibblereport", "Опишите вашу проблему:", replyMarkup: new ForceReplyMarkup(), replyToMessageId: messageId);
         }
     }    
 }
