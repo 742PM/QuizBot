b497db92135bacbd2e5a48f4c5a5bd52fbef3f78
insomnia* <budljanskiy@mail.ru>
2019-05-12 09:30:08 +0500

    add more user report info;

--- QuizBot/QuizBotCore/Commands/ReportTaskCommand.cs
+++ QuizBot/QuizBotCore/Commands/ReportTaskCommand.cs
@@ -29,6 +29,9 @@ namespace QuizBotCore
         public async Task ExecuteAsync(Chat chat, TelegramBotClient client, ServiceManager serviceManager)
         {
             var user = serviceManager.userRepository.FindByTelegramId(chat.Id);
+            var reportUserInfo = $"Report message from: {chat.Id};" +
+                                 $"UserId: {user.Id}, State: {user.CurrentState}";
+            await client.SendTextMessageAsync(ReportContact, reportUserInfo);
             await client.ForwardMessageAsync(ReportContact, chat.Id, messageId);
             await client.ForwardMessageAsync(ReportContact, chat.Id, user.MessageId);
             await client.SendTextMessageAsync(chat.Id, DialogMessages.ReportThanks);
