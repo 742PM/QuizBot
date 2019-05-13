fbd68fe9bdf89c8498082e3532856e93908f2a6e
insomnia* <budljanskiy@mail.ru>
2019-05-13 09:20:24 +0500

    better report info bson fixes;

--- QuizBot/QuizBotCore/States/ReportState.cs
+++ QuizBot/QuizBotCore/States/ReportState.cs
@@ -1,16 +1,21 @@
+using MongoDB.Bson.Serialization.Attributes;
 using QuizRequestService.DTO;
 
 namespace QuizBotCore.States
 {
     class ReportState : State
     {
+        [BsonElement]
         public readonly TopicDTO TopicDto;
+        
+        [BsonElement]
         public readonly LevelDTO LevelDto;
-
+        
+        [BsonConstructor]
         public ReportState(TopicDTO topicDto, LevelDTO levelDto)
         {
-            this.TopicDto = topicDto;
-            this.LevelDto = levelDto;
+            TopicDto = topicDto;
+            LevelDto = levelDto;
         }
     }
 }
